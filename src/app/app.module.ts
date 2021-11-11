0cc4c94564bb444defa816f03386d2b44ceb80dd
haythem benSalah <haythem.bensalah@insat.u-carthage.tn>
2021-11-11 14:28:30 +0100

    add Material-UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FilterComponent } from './filter/filter.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModel } from './material.model';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { FilterComponent } from './filter/filter.component';
     FilterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModel
   ],
   providers: [],
   bootstrap: [AppComponent]

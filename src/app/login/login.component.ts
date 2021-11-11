9aa781ffc26f9f2eca237bf12fe2e9048c503420
taha361 <tahayassinebenameur36@gmail.com>
2021-11-11 14:32:48 +0100

    change1

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,8 +8,28 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
+  password="";
+  email="";
+  error: string = null;
+   constructor(public authService : AuthService ,private dialog: MatDialog) { }
+ 
+   ngOnInit(): void {
+   }
+ 
+   login(form : NgForm) {
+     const email = form.value.email;
+     const password = form.value.password;
+     
+ this.authService.login(email, password);
+   
+ form.reset();
+ 
+ 
+ }
+ 
+ 
+ 
+ 
+  
 
 }

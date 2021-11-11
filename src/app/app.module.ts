7a46f7ca35e21a77c1849aca98593979677f5ca3
haythem benSalah <haythem.bensalah@insat.u-carthage.tn>
2021-11-11 14:14:22 +0100

    login and filter component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule
